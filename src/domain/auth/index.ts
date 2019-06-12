e495e50afbc2c0c4b131b958f582261771f0cd3a
AkiraTameto <akira.tametou@gmail.com>
2019-06-13 08:38:36 +0900

    [fix] 暫定的にsetOwnIdを追加

--- src/domain/auth/index.ts
+++ src/domain/auth/index.ts
@@ -1,13 +1,18 @@
-import { auth } from '../../firebase/index'
-import { isDebug } from '../../debug';
+import { auth } from "../../firebase/index";
+import { isDebug } from "../../debug";
 
+let currentUserUid = "3";
 export function getOwnId(): string {
   if (isDebug()) {
-    return "1"
+    return currentUserUid;
   }
-  const { currentUser } = auth()
+  const { currentUser } = auth();
   if (currentUser) {
-    return currentUser.uid
+    return currentUser.uid;
   }
-  throw Error("未ログイン状態で getOwnId を実行したのでエラーを投げました")
+  throw Error("未ログイン状態で getOwnId を実行したのでエラーを投げました");
+}
+
+export function setOwnId(uid: string) {
+  currentUserUid = uid;
 }
