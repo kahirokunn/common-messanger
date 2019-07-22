6e208f7796834dc86edf74709afc5b6f1ed6adb2
k-okina <okinakahiro@gmail.com>
2019-07-22 22:12:43 +0900

    必要な関数を全てexportした

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { installApp } from "./firebase";
+export { isTextMessage, isNoteMessage, isImageMessage } from "./domain/message"
+export { installApp } from "./firebase"
+export { sendImageMessage, sendNoteMessage, sendTextMessage } from "./command/message"
+export { MessageObserver } from "./query/message"
