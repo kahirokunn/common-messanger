ebd8b0b597c8aed15cebcfa412872c83ae0d68c8
k-okina <okinakahiro@gmail.com>
2019-07-26 17:41:15 +0900

    [fix] read message commandでunread message segmentをsliceするようにした

--- src/command/readMessage/index.ts
+++ src/command/readMessage/index.ts
@@ -1,28 +1,87 @@
-import { firebase, firestore } from "../../firebase";
-import { getAccountRoomPath } from "../../firebase/collectionSchema";
-import { Room } from "../../domain/account/room";
+import { firebase, firestore } from "../../firebase"
+import { getUnreadMessageSegmentPath } from "../../firebase/collectionSchema"
+import { UnreadMessageSegment } from "../../domain/account/unreadMessageSegment"
+import { Id } from "../../firebase/type"
+import { Message } from "../../domain/message/message"
+import { UnreadMessageSegmentDoc } from "../../query/timeline/unreadMessageSegments";
 
-
-export async function readMessage(room: Room): Promise<void> {
-  await updateRoom(room)
-}
-
-function updateRoom(room: Room) {
+export async function readMessage(roomId: Id, beginAt: Date, endAt: Date): Promise<void> {
   const currentUser = firebase.auth().currentUser
-  if (currentUser) {
-    return firestore
-      .collection(getAccountRoomPath(currentUser.uid))
-      .doc(room.id)
-      .set(mapEntityToDTO(room))
+  if (!currentUser) {
+    throw Error('failed to get current user from firebase auth sdk')
   }
-  throw Error('failed to get current user from firebase auth sdk')
+
+  const unreadMessageSegments = await getAllUnreadMessageSegmentSnapshot(currentUser.uid, roomId)
+  const batch = firestore.batch()
+  removeReadMessages({
+    roomId,
+    batch,
+    accountId: currentUser.uid,
+    beginAt,
+    endAt,
+    unreadMessageSegments
+  })
+  await batch.commit()
 }
 
-type RoomDTO = Omit<Room, 'id'>
+type Input = {
+  batch: firebase.firestore.WriteBatch,
+  accountId: Id,
+  roomId: Id,
+  beginAt: Date,
+  endAt: Date,
+  unreadMessageSegments: UnreadMessageSegmentWithId
+}
+function removeReadMessages(input: Input) {
+  const removeTargets = getRemoveTargets(input)
+  Object.keys(removeTargets).forEach(segmentId => {
+    Object.keys(removeTargets[segmentId]).forEach(messageId => {
+      input.batch.update(
+        firestore
+          .collection(`${getUnreadMessageSegmentPath(input.accountId, input.roomId)}`)
+          .doc(segmentId),
+        { [messageId]: firebase.firestore.FieldValue.delete() }
+      )
+    })
+  })
+}
 
-function mapEntityToDTO(room: Room): RoomDTO {
-  return {
-    beginAt: room.beginAt,
-    endAt: room.endAt
-  }
+function getRemoveTargets(input: Input) {
+  const removeTargetSegments: UnreadMessageSegmentWithId = {}
+  Object.keys(input.unreadMessageSegments).forEach(segmentId => {
+    removeTargetSegments[segmentId] = {}
+    const unreadMessageSegment = input.unreadMessageSegments[segmentId]
+    const messages = mapToMessages(unreadMessageSegment)
+    messages
+      .filter(message => input.beginAt <= message.createdAt && message.createdAt <= input.endAt)
+      .forEach(message => removeTargetSegments[segmentId][message.id] = message)
+  })
+  return removeTargetSegments
+}
+
+function mapToMessages(unreadMessageSegment: UnreadMessageSegment): Message[] {
+  return Object.keys(unreadMessageSegment).map(messageId => ({
+    id: messageId,
+    ...unreadMessageSegment[messageId]
+  } as Message))
+}
+
+type UnreadMessageSegmentWithId = { [id: string]: UnreadMessageSegment }
+async function getAllUnreadMessageSegmentSnapshot(accountId: Id, roomId: Id): Promise<UnreadMessageSegmentWithId> {
+  const unreadMessageSegmentsQuerySnap = await firestore
+    .collection(getUnreadMessageSegmentPath(accountId, roomId))
+    .get()
+  const output: UnreadMessageSegmentWithId = {}
+  unreadMessageSegmentsQuerySnap.forEach(unreadMessageSegmentDocSnap => {
+    output[unreadMessageSegmentDocSnap.id] = {}
+    const unreadMessageSegmentDoc: UnreadMessageSegmentDoc = unreadMessageSegmentDocSnap.data()
+    Object.keys(unreadMessageSegmentDoc).forEach(messageId => {
+      output[unreadMessageSegmentDocSnap.id][messageId] = {
+        ...unreadMessageSegmentDoc[messageId],
+        createdAt: unreadMessageSegmentDoc[messageId].createdAt.toDate()
+      }
+    })
+  })
+
+  return output
 }
