b63523d842b73627e3c6394238d031deda78a19a
k-okina <okinakahiro@gmail.com>
2019-09-27 15:25:13 +0900

    [fix] テストをsetupする為のhelper moduleをそのテストを作成

---
+++ __tests__/helper/factory/room.ts
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase/app'
+import { Room, ROOM_TYPE } from '../../../src/domain/message/room'
+
+let id = 0
+
+type Base = Partial<Room> & { memberIds: Room['memberIds'] }
+
+export function roomFactory<T extends Base>(overrides: T): Room & T {
+  id += 1
+  return {
+    id,
+    name: `${id}nth room`,
+    roomType: ROOM_TYPE.DIRECT,
+    memberIds: overrides.memberIds,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+    ...overrides,
+  }
+}
