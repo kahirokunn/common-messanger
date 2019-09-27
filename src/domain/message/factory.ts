1a323274d144c52c9241098280ceef133870b194
k-okina <okinakahiro@gmail.com>
2019-09-27 15:26:10 +0900

    [fix] テスト中にfirebase.authが使えないので、mockするようにした

--- src/domain/message/factory.ts
+++ src/domain/message/factory.ts
@@ -1,21 +1,17 @@
-import * as firebase from 'firebase/app'
 import { TextMessage, NoteMessage, ImageMessage, MESSAGE_TYPE } from './message'
 import { Id } from '../../firebase/type'
+import { getAccountId } from '../../firebase/user'
 
 type InputText = Omit<Omit<Omit<Omit<TextMessage, 'type'>, 'createdAt'>, 'sentFromAccountId'>, 'id'>
 type InputNote = Omit<Omit<Omit<Omit<NoteMessage, 'type'>, 'createdAt'>, 'sentFromAccountId'>, 'id'>
 type InputImage = Omit<Omit<Omit<Omit<ImageMessage, 'type'>, 'createdAt'>, 'sentFromAccountId'>, 'id'>
 
 function setSharedProperty<T>(input: T): T & { createdAt: Date; sentFromAccountId: Id } {
-  const { currentUser } = firebase.auth()
-  if (currentUser) {
-    return {
-      ...input,
-      createdAt: new Date(),
-      sentFromAccountId: currentUser.uid,
-    }
+  return {
+    ...input,
+    createdAt: new Date(),
+    sentFromAccountId: getAccountId(),
   }
-  throw Error('failed to get current user from firebase auth sdk')
 }
 
 export function textMessageFactory(input: InputText): Omit<TextMessage, 'id'> {
